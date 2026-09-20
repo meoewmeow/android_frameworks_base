f9f831fe0c713593a0a31d284c36e6a266ad5c27
rime <mail@rime.cyou>
2026-09-20 22:35:30 +0300

    Privacy: add no storage restrict support
    Donor: Evolution X frameworks_base@bka (Settings.Global.NO_STORAGE_RESTRICT).
    ExternalStorageProvider.shouldBlockDirectoryFromTree returns false while the
    setting is enabled.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -22778,6 +22778,12 @@ public final class Settings {
          */
         public static final String HIDE_SCREEN_CAPTURE_STATUS = "hide_screen_capture_status";
 
+        /**
+         * Whether the Storage Access Framework folder restrictions are removed.
+         * @hide
+         */
+        @Readable
+        public static final String NO_STORAGE_RESTRICT = "no_storage_restrict";
     }
 
     /**
